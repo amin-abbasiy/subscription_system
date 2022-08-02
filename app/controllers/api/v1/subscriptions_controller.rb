c9483ac901de1940b391fd2768959981d4be4d8e
a.abbasi <a.abbasi@roundtableapps.com>
2022-08-03 00:18:06 +0430

    add home page and calculate cost

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
+end
--- app/models/template.rb
+++ app/models/template.rb
@@ -4,4 +4,8 @@ class Template < ApplicationRecord
 
   has_many :sections, dependent: :destroy
   has_many :comments
+
+  scope :list, -> (per, user_id, offset) { where(user_id: user_id).order_by(:created_at)
+                                                                  .offset(offset).limit(per)
+                                                                  .select(:title, :comment_count, :section_count) }
 end
